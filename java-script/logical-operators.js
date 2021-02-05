6f0aeec195bc092bfb13c0b697ec7b22be44f77e
Aleksandra Stoliarova <alek.stoliarova@yandex.ru>
2021-02-05 22:26:24 +0100

    Or and not (logical operations)

---
+++ java-script/logical-operators.js
@@ -0,0 +1,27 @@
+// const password = prompt("enter you password");
+// if(password.length >= 6 && password.indexOf(" ") === -1) {
+//     console.log("valid password");
+// } else {
+//     console.log("incorrect password");
+// } 
+
+// const age = 80;
+// if(age >=0 && age < 5 || age >= 65) {
+//     console.log('FREE');
+// } else if (age >= 5 && age < 10) {
+//     console.log('$10')
+// } else if (age >= 10 && age < 65 ) {
+//     console.log('$20')
+// } else {
+//     console.log("invalid age")
+// }
+
+// const firstName = prompt("enter your first name");
+// if (!firstName) {
+//     const firstName = prompt("try again");
+// }
+
+const age = 45;
+if(!(age >=0 && age < 5 || age >= 65)) {
+      console.log("you are not a baby or a senior")
+    }
