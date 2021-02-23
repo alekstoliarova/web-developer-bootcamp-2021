4f2cf90673f3e40610abd80998fe41174f1711ff
Aleksandra Stoliarova <alek.stoliarova@yandex.ru>
2021-02-23 20:49:58 +0100

    Break Keyword

--- java-script/while-loop.js
+++ java-script/while-loop.js
@@ -4,10 +4,28 @@
 //     console.log(count)
 // }
 
-const secret = "BabyHippo";
+// const secret = "BabyHippo";
 
-let guess = prompt("enter the secret code..");
-while (guess !== secret){
-    guess = prompt("enter the secret code..");
+// let guess = prompt("enter the secret code..");
+// while (guess !== secret){
+//     guess = prompt("enter the secret code..");
+// }
+// console.log("congrats you got the secret")
+
+
+// let input = prompt("Hey, say something!")
+// while(true) {
+//    input = prompt(input);
+//    if(input.toLowerCase() === "stop copying me") {
+//        break;
+//    }
+// }
+// console.log("ok u win!") 
+
+
+for (let i = 0; i <1000; i++) {
+    console.log(i);
+    if(i === 100) {
+        break;
+    }
 }
-console.log("congrats you got the secret")
